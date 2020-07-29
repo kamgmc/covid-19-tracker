d25b33b70813f9707f44d7c6669095d04dba0351
Kevin Martinez <kamgmc@gmail.com>
2020-07-29 14:17:56 -0700

    [Style] Add color to board titles

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -6,8 +6,8 @@
         class="mx-auto"
         raised
       >
-        <v-card-subtitle class="pb-0">Infected</v-card-subtitle>
-        <v-card-title class="pt-0" v-if="infected">{{new Intl.NumberFormat().format(infected)}}</v-card-title>
+        <v-card-subtitle class="pb-0 font-weight-bold black--text">Infected</v-card-subtitle>
+        <v-card-title class="pt-0 blue--text" v-if="infected">{{new Intl.NumberFormat().format(infected)}}</v-card-title>
         <v-card-subtitle v-if="lastUpdated">{{new Date(lastUpdated).toDateString()}}</v-card-subtitle>
         <v-card-text>Number of infected caused by COVID-19.</v-card-text>
         <v-card-actions class="bar info"></v-card-actions>
@@ -18,8 +18,8 @@
         class="mx-auto"
         raised
       >
-        <v-card-subtitle class="pb-0">Recovered</v-card-subtitle>
-        <v-card-title class="pt-0" v-if="recovered">{{new Intl.NumberFormat().format(recovered)}}</v-card-title>
+        <v-card-subtitle class="pb-0 font-weight-bold black--text">Recovered</v-card-subtitle>
+        <v-card-title class="pt-0 success--text" v-if="recovered">{{new Intl.NumberFormat().format(recovered)}}</v-card-title>
         <v-card-subtitle v-if="lastUpdated">{{new Date(lastUpdated).toDateString()}}</v-card-subtitle>
         <v-card-text>Number of recovered caused by COVID-19.</v-card-text>
         <v-card-actions class="bar success"></v-card-actions>
@@ -30,8 +30,8 @@
         class="mx-auto"
         raised
       >
-        <v-card-subtitle class="pb-0">Deaths</v-card-subtitle>
-        <v-card-title class="pt-0" v-if="deaths">{{new Intl.NumberFormat().format(deaths)}}</v-card-title>
+        <v-card-subtitle class="pb-0 font-weight-bold black--text">Deaths</v-card-subtitle>
+        <v-card-title class="pt-0 error--text" v-if="deaths">{{new Intl.NumberFormat().format(deaths)}}</v-card-title>
         <v-card-subtitle v-if="lastUpdated">{{new Date(lastUpdated).toDateString()}}</v-card-subtitle>
         <v-card-text>Number of deaths caused by COVID-19.</v-card-text>
         <v-card-actions class="bar error"></v-card-actions>
